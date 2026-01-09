bf8c3dd06f2b8c110f6218518c9ea5ef5c95b724
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2026-01-08 21:46:33 -0300

    Introduzindo lista

---
+++ Tabela-01.js
@@ -0,0 +1,13 @@
+// Experimento com tabelas em JavaScript
+
+let text = 'Este é um texto para teste.'
+let arr_teste = ['Peloponeso', 'Marmara', 'Tartus', 127.31, 'Polinésia',
+    'Tajikistan', 'Lucknow']
+
+// document.getElementById('par_dados').innerHTML = text
+document.getElementById('par_dados1').innerHTML = arr_teste
+
+for (let i = 0; i < arr_teste.length; i++) {
+    document.getElementById('par_dados2').innerHTML += `${arr_teste[i] + '<br>'} ` 
+}
+
