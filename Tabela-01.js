f900d9e56d1e4ba62857779443f5f76f19114040
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2026-01-10 12:57:43 -0300

    Estilo da tabela

--- Tabela-01.js
+++ Tabela-01.js
@@ -1,15 +1,40 @@
-// Experimento com tabelas em JavaScript
+// Tabelas em JavaScript
 
-let text = 'Este é um texto para teste.'
-let arr_teste = ['Peloponeso', 'Marmara', 'Tartus', 127.31, 'Polinésia',
-    'Tajikistan', 'Lucknow']
+// Definir dados:
+  const dados = [
+    { cidade: 'Kolkhata', pais: 'Índia', continente: 'Ásia' },
+    { cidade: 'Nairobi', pais: 'Kenya', continente: 'África' },
+    { cidade: 'Lima', pais: 'Peru', continente: 'América do Sul' }, 
+    { cidade: 'Tashkent', pais: 'Uzbekistão', continente: 'Ásia' },
+    { cidade: 'Tegucigalpa', pais: 'Honduras', continente: 'América Central' },
+    { cidade: 'Calgary', pais: 'Canadá', continente: 'América do Norte' },
+  ]
 
-// document.getElementById('par_dados').innerHTML = text
-document.getElementById('par_dados1').innerHTML = arr_teste
+  // Criar uma tabela:
+  const tabela = document.createElement("table")
 
-for (let i = 0; i < arr_teste.length; i++) {
-    document.getElementById('par_dados2').innerHTML += `${arr_teste[i] + '<br>'} ` 
-}
+  // Acrescentar cabeçalho:
+  const headers = ['Cidade', 'País', 'Continente']
+  const headerRow = document.createElement("tr")
 
-//-------------------------------------------------------------------------------
-// Tabela:
+  headers.forEach(elem => {
+    const head = document.createElement("th")
+    head.textContent = elem
+    headerRow.appendChild(head)
+  })
+  tabela.appendChild(headerRow)
+
+  // Acrescentar dados:
+  dados.forEach(item => {
+    const row = document.createElement("tr")
+    Object.values(item).forEach(value => {
+      const cell = document.createElement("td")
+      cell.textContent = value
+      row.appendChild(cell)
+    })
+    tabela.appendChild(row)
+  })
+
+  // Inserir a tabela no DOM:
+  document.getElementById("div_tabela").appendChild(tabela)
+
